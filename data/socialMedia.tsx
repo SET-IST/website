19dd93d48da054013e655fd0d9d2383af8b70bb6
Samuel Barata <samuel.barata@tecnico.ulisboa.pt>
2025-02-19 13:50:24 +0000

    change Twitter icon to X

--- data/socialMedia.tsx
+++ data/socialMedia.tsx
@@ -1,10 +1,20 @@
-import { SiFacebook, SiInstagram, SiTwitter, SiYoutube } from 'react-icons/si'
+import { SiFacebook, SiInstagram, SiX, SiYoutube, SiLinkedin, SiTiktok } from 'react-icons/si'
 
 const socialMedia = [
   {
-    name: 'Twitter',
-    href: 'https://twitter.com/SET_ISTTagus',
-    logo: <SiTwitter color="white" className="w-9 h-9" />,
+    name: 'LinkedIn',
+    href: 'https://www.linkedin.com/company/settaguspark',
+    logo: <SiLinkedin color="white" className="w-9 h-9" />,
+  },
+  {
+    name: 'Instagram',
+    href: 'https://www.instagram.com/set.ist/',
+    logo: <SiInstagram color="white" className="w-9 h-9" />,
+  },
+  {
+    name: 'TikTok',
+    href: 'https://www.tiktok.com/@set.tecnico',
+    logo: <SiTiktok color="white" className="w-9 h-9" />,
   },
   {
     name: 'Facebook',
@@ -12,15 +22,15 @@ const socialMedia = [
     logo: <SiFacebook color="white" className="w-9 h-9" />,
   },
   {
-    name: 'Youtube',
-    href: 'https://www.youtube.com/user/ISTTagusSET',
-    logo: <SiYoutube color="white" className="w-9 h-9" />,
+    name: 'X',
+    href: 'https://x.com/SET_ISTTagus',
+    logo: <SiX color="white" className="w-9 h-9" />,
   },
-  {
-    name: 'Instagram',
-    href: 'https://www.instagram.com/set.ist/',
-    logo: <SiInstagram color="white" className="w-9 h-9" />,
-  },
+  // {
+  //   name: 'Youtube',
+  //   href: 'https://www.youtube.com/user/ISTTagusSET',
+  //   logo: <SiYoutube color="white" className="w-9 h-9" />,
+  // },
 ]
 
 export { socialMedia }
