1e7d6473e8b7750ad7a6e30ae7348d1e4acc2775
Samuel Barata <samuel.barata@tecnico.ulisboa.pt>
2025-02-19 21:13:35 +0000

    code cleanup

--- components/PrizeDialog/components/ModalContent.tsx
+++ components/PrizeDialog/components/ModalContent.tsx
@@ -23,76 +23,37 @@ export function ModalContent() {
   // Fetch user data
   const { data, isLoading: isUserLoading, isError: isUserError } = useProfile()
 
-  console.log("data: ", data)
-  console.log("isUserLoading: ", isUserLoading)
-  console.log("isUserError: ", isUserError)
-
   // Fetch award data
   const {
     data: awardData,
     isSuccess: awardLoaded,
     error: awardError,
-    refetch,
-    isLoading: isAwardLoading,
   } = useAward()
-
-  console.log("awardLoading: ", isAwardLoading)
-  console.log("awardData: ", awardData)
-  console.log("awardLoaded: ", awardLoaded)
-  console.log("awardError: ", awardError)
 
   // Check if user has enough points for a redeem
   const notEnoughPoints = awardError?.response?.status === 400
-  console.log("notEnoughPoints: ", notEnoughPoints)
 
   const user = data as StudentProfile
   const redemptionSettings = useQuery<RedemptionSettings>(['redemptionSettings'], () => fetchRedemptionSettings())
 
-
   // Load awards list
   const { data: awardsListData, isLoading: isAwardsListLoading, isError: isAwardsListError, isSuccess: wheel_data_loaded } = useQuery<Award[]>(['awardsList'], () => fetchAwardsList())
-  console.log("awardsList: ", awardsListData)
 
   // Create data for the wheel
   const data_wheel: WheelDataType[] = Array.isArray(awardsListData) && awardData?.type
   ? awardsListData?.filter((award) => award.type === awardData.type)?.map((award) => ({ option: award.name, optionSize: award.amountAvailable }))
   : [{ option: "Carregando..." }];
 
-  // const data_wheel: WheelDataType[] = awardsListData ? awardsListData.map((award) => ({ option: award?.name })) : [{"option": "Carregando..."}]
-  console.log("data_wheel: ", data_wheel)
-  console.log("lastAwardToken:", lastAwardToken);
-  console.log("awardData:", awardData?.id);
-
-  // const isVisible = useBoundStore((state) => state.redeemModalVisible)
-  // console.log("isVisible:", isVisible);
-
-  // if (isVisible && !awardData) {
-  //     refetch() // Fetch only when modal opens
-  //     console.log("🕰️ refetching...")
-  // }
-
   // Checkes if the award token has been previously seen
   const isSameToken = lastAwardToken === awardData?.id;
   console.log(isSameToken ? "same" : "different");
 
-
-  // Debug Wheel stoped
-  useEffect(() => {
-    console.log("Wheel Stopped:", wheelStopped); // ✅ Debugging
-  }, [wheelStopped]); // Runs whenever wheelStopped changes
-
-
   // Starts the wheel when the award is loaded
   useEffect(() => {
     if (awardLoaded && data_wheel.length > 1 && !notEnoughPoints) {
-      console.log("setting up wheel...");
       const prizeIndex = data_wheel.findIndex((item) => item.option === awardData?.award.name);
-      console.log("prizeIndex:", prizeIndex);
-      console.log("target:" , data_wheel[prizeIndex].option);
       setPrizeNumber(prizeIndex);
-      console.log("wheel setup done");
       setMustSpin(true);
-      console.log("wheel started");
     }
   }, [awardLoaded, data_wheel]);
 
@@ -160,7 +121,7 @@ export function ModalContent() {
                             setTimeout(() => {
                               setMustSpin(false);
                               setWheelStopped(true);
-                            }, 2000); // Delay of 2 seconds
+                            }, 2000);
                           }}
                           spinDuration={0.70}
                         />
@@ -170,27 +131,6 @@ export function ModalContent() {
                     )}
                   </div>
 
-                {/* {lastAwardToken == awardData?.id ? (
-                  <PrizeCard award={awardData} />
-                ) :( */}
-                  {/* <div>
-                    {!wheelStopped ? (
-                      <Wheel
-                        mustStartSpinning={mustSpin}
-                        prizeNumber={prizeNumber}
-                        data={data_wheel}
-                        onStopSpinning={() => { // wait 1 second for readind the prize, and then hide wheel and show qrcode
-                          setTimeout(() => {
-                            setWheelStopped(true);
-                          }, 1000);
-                        }}
-                        spinDuration={0.70}
-                      />
-                    ) : (
-                      <PrizeCard award={awardData} />
-                    )}
-                  </div> */}
-                {/* )} */}
                 </div>
                 )}
               </Transition>
@@ -198,74 +138,7 @@ export function ModalContent() {
           </div>
             )}
           </div>
-
-          {/* <div className="flex flex-col gap-5">
-            <div className="flex flex-col items-center gap-4">
-              <Transition
-                mounted={true} //{awardLoaded}
-                transition="slide-up"
-                duration={200}
-                timingFunction="ease"
-                keepMounted
-              >
-              {(styles) => (
-                <div
-                  className="w-full flex flex-col gap-4 items-center"
-                  style={styles}
-                >
-                  <div>
-                    {isSameToken ? (
-                      <PrizeCard award={awardData} />
-                    ) : (
-                      !wheelStopped ? (
-                        <Wheel
-                          mustStartSpinning={mustSpin}
-                          prizeNumber={prizeNumber}
-                          data={data_wheel}
-                          onStopSpinning={() => {
-                            setTimeout(() => {
-                              setWheelStopped(true);
-                            }, 2000); // Delay of 2 seconds
-                          }}
-                          spinDuration={0.70}
-                        />
-                      ) : (
-                        <PrizeCard award={awardData} />
-                      )
-                    )}
-                  </div> */}
-
-                {/* {lastAwardToken == awardData?.id ? (
-                  <PrizeCard award={awardData} />
-                ) :( */}
-                  {/* <div>
-                    {!wheelStopped ? (
-                      <Wheel
-                        mustStartSpinning={mustSpin}
-                        prizeNumber={prizeNumber}
-                        data={data_wheel}
-                        onStopSpinning={() => { // wait 1 second for readind the prize, and then hide wheel and show qrcode
-                          setTimeout(() => {
-                            setWheelStopped(true);
-                          }, 1000);
-                        }}
-                        spinDuration={0.70}
-                      />
-                    ) : (
-                      <PrizeCard award={awardData} />
-                    )}
-                  </div> */}
-                {/* )} */}
-                {/* </div>
-                )}
-              </Transition>
-            </div>
-          </div> */}
         </div>
-
-        {/* {useEffect(() => {
-          console.log("Wheel Stopped:", wheelStopped);
-        }, [wheelStopped])} */}
 
         {wheelStopped && (
           <div>
--- lib/frontend/hooks/student.ts
+++ lib/frontend/hooks/student.ts
@@ -51,41 +51,17 @@ export const useAward = () => {
     queryKey: ['Award'],
     queryFn: fetchAward,
     enabled: true,
-    // cacheTime: Infinity,
-    // staleTime: Infinity,
-    refetchOnWindowFocus: true, // FIXME the page refetchs on window focus, even with false (the qrcode changes after redeem without the whell, if the user has the redeem modal open)
+    cacheTime: Infinity,
+    staleTime: Infinity,
+    refetchOnWindowFocus: true,
     refetchInterval: (data, query) => {
       return !query.state.error ? 800 : false
     },
-    // retry: false, // ⬅️ Only retry twice before stopping
-    // retryDelay: (attempt) => Math.min(1000 * 5 ** attempt, 30000), // Exponential backoff
-    //retry: (failureCount, error) => false,
     onSuccess: (data) => {
-      // if (lastToken === data?.id) {
-      //   console.log("same token recieved from api")
-      // } else {
-      //   console.log("different token recieved from api")
-      // }
-      console.log("fetchAward data hooks: ", data)
       setAwardToken(data?.id || '')
     },
     onError: (error) => {
       console.error("fetchAward error hooks: ", error)
-      // Handle error state here if needed
     },
-    // retry: (failureCount, err) => {
-    //   if (err.response?.status === 400) {
-    //     console.log('This works')
-    //     return true; // do not retry, trigger error
-    //   }
-    //   return true; // retry for other errors
-    // },
   })
 }
-
-
-// const setAwardToken = useBoundStore((state) => state.setAwardToken)
-
-// useEffect(() => {
-//   fetchAward().then(setAwardToken)
-// }, [])
